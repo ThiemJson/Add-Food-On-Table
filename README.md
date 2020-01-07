94795f511726c6ea2ce34c50e5a831dffa4676c9
thiemjson <thiemjson@gmail.com>
2020-01-07 21:24:31 +0700

    Commiting again

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ Project:  Add foods to Foods-Table. ( render on HTML attr) (owner: ThiemJson)
 
 s1: Create table, button and input-form
 s2: Creat file JS suppport to ADD button
+s3:  Create file Db.json, add lib Axios on index.html, create buttin ADDFood
--- main.js
+++ main.js
@@ -1,5 +1,26 @@
 var foodInput = document.getElementById("foods-input");
 var btnAdd = document.getElementById("btn-add-foods");
 var foodTable = document.getElementById("foods-table");
+var url = "http://localhost:9081/foods"
+
+//Bind Event
+btnAdd.addEventListener('click',addItem);
 
 
+//Declare Funciton
+function addItem(){
+
+};
+function render(items){
+    let content = items.map((item)=>{
+        return '<tr><td> '+item.name+'</td></tr>';
+    })
+    foodTable.innerHTML = content.join(" ");
+};
+
+
+//Excuted function
+axios.get(url).then((resnponse)=>{
+    let data = resnponse.data;
+    render(data);
+})
